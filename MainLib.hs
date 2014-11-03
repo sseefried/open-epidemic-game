bf7958ad7673aa8291287b278d7183ce76aea7aa
Sean Seefried <sean.seefried@gmail.com>
2014-11-03 12:06:00 +1100

    Add missing MainLib.hs

---
+++ MainLib.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ForeignFunctionInterface, CPP #-}
+{-# OPTIONS_GHC -Wall #-}
+
+module MainLib where
+
+-- friends
+import Game
+import Backend.SDL as B
+
+screenWidth, screenHeight :: Int
+screenWidth  = 800
+screenHeight = 480
+
+w, h :: Double
+w = fromIntegral screenWidth
+h = fromIntegral screenHeight
+
+foreign export ccall "haskell_main" main :: IO ()
+
+main :: IO ()
+main = do
+  gs     <- newGameState (screenWidth, screenHeight)
+  besRef <- B.initialize "Epidemic" screenWidth screenHeight gs
+  B.mainLoop besRef handleEvent
