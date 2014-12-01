fd88351c7b91b978c11cbbca7a33a01eb7c662e0
Rauri Rochford <raurir@gmail.com>
2014-12-01 22:13:47 +1100

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export PATH=$HOME/.cabal/bin:$PATH
 rm -f cabal.sandbox.config
 rm -rf .cabal-sandbox
 cabal sandbox init
-cabal intall gtk2hs-buildtools
+cabal install alex happy
+cabal install gtk2hs-buildtools
 cabal sandbox add-source $HOME/code/forked-git/hs-sdl2-mixer
 cabal sandbox add-source $HOME/code/forked-git/hsSDL2
-cabal install -p --enable-executable-profiling -j8
+cabal install  -j8 -fnosound
