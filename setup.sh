b99e1c0020102fa496d7db0347f371e0aef202d0
Rauri Rochford <raurir@gmail.com>
2014-12-01 21:54:32 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 rm -f cabal.sandbox.config
 rm -rf .cabal-sandbox
 cabal sandbox init
-cabal sandbox add-source /Users/sseefried/code/forked-git/hs-sdl2-mixer
-cabal sandbox add-source /Users/sseefried/code/forked-git/hsSDL2
+cabal intall gtk2hs-buildtools
+cabal sandbox add-source $HOME/code/forked-git/hs-sdl2-mixer
+cabal sandbox add-source $HOME/code/forked-git/hsSDL2
 cabal install -p --enable-executable-profiling -j8
