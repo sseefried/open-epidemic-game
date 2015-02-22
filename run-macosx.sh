24bbcb393562b7e245b84d6723fc8de804b3a67d
Sean Seefried <sean.seefried@gmail.com>
2015-02-22 15:34:02 +1100

    Add script to run game on Mac OS X. Saves me a lot of typing

---
+++ run-macosx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -gt 0 ]; then
+  case "$1" in
+    --debug)
+      DEBUG_FLAG=-fdebug-game
+    ;;
+  esac
+
+fi
+
+cabal configure $DEBUG_FLAG
+cabal build exe:Epidemic
+macosx-app dist/build/Epidemic/Epidemic
+open Epidemic.app
