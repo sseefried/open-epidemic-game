2f01283d716c3649453aba3bfacce2f2486341a1
Sean Seefried <sean.seefried@gmail.com>
2015-02-25 21:38:03 +1100

    Beginings of a module for procedurally generated music.

--- src/ProceduralMusic.hs
+++ src/ProceduralMusic.hs
@@ -7,6 +7,7 @@ import qualified Graphics.UI.SDL.Mixer.Types as M
 import qualified Graphics.UI.SDL.Keycode     as SK
 import           System.Exit
 import           Foreign.Marshal.Alloc
+import           Foreign.Marshal.Utils
 import           Control.Monad
 import           Foreign.Storable
 import           Foreign.ForeignPtr
@@ -16,6 +17,8 @@ import           GHC.Word
 import           Data.Int
 import           Data.IORef
 import           Data.Time
+import           Control.Concurrent
+
 
 w, h :: Int
 w = 320
@@ -28,38 +31,38 @@ outputChannels, mixChannels, bytesPerSample :: Int
 outputChannels = 2 -- 2 is for stereo, 1 is for mono
 mixChannels = 10
 bytesPerSample  = 2
-
+sampleRate = 48000
 ----------------------------------------------------------------------------------------------------
-data State = State { stChunkQueue          :: [SoundChunk]
-                   , stCurrentChunk        :: Maybe SoundChunk
-                   , stCurrentChunkElapsed :: Double
-                   , stLastTime            :: UTCTime
+data State = State { stLastTime            :: UTCTime
                    , stWindow              :: S.Window -- you need to keep a reference to this
                    }
 
-data SoundChunk = SoundChunk { scChunk :: M.Chunk, scDuration :: Time }
 ----------------------------------------------------------------------------------------------------
-type MusicHook = Ptr Word8 -> Int -> IO ()
-
-----------------------------------------------------------------------------------------------------
+hook :: IORef Time -> M.MusicHook
+hook ref ptr bytes = do
+  t <- readIORef ref
+  --
+  -- If you were asked for 8192 bytes and the sampleRate was 44100, outPutChannels == 2
+  -- and bytesPerSample == 2, then 4 bytes would be used for each 2-channel sample.
+  -- 8192 / (44100 * 4) == 0.0464s
+  --
+  let duration = fromIntegral bytes / fromIntegral (sampleRate*outputChannels*bytesPerSample)
+  -- Now get the bytes
+  createChunkFromSound ptr sampleRate bytes sin440 t
+  writeIORef ref $ t + duration
 
 main :: IO ()
 main = do
   S.init [S.InitVideo, S.InitAudio]
   window  <- S.createWindow "Procedural Music" (S.Position 0 (-500)) (S.Size w h) [S.WindowShown]
-  let sampleRate = 48000
-  M.openAudio sampleRate S.AudioS16Sys outputChannels 8192
+  M.openAudio sampleRate S.AudioS16Sys outputChannels (2^16)
   M.allocateChannels mixChannels
-
-
-  --let f :: Sound -> Time -> IO SoundChunk
-  --    f snd startTime = createChunkFromSound sampleRate 1 (snd `fromTime` startTime)
-  --chunkQueue <- mapM (f sin440) (take 1 [0..])
+  soundTimeRef <- newIORef 0
+  -- now copy the chunk to ptr
+  M.hookMusic (hook soundTimeRef)
+  S.unlockAudio
   t <- getCurrentTime
-  sref <- newIORef $ State { stChunkQueue = []
-                           , stCurrentChunk = Nothing
-                           , stCurrentChunkElapsed = 0
-                           , stLastTime = t
+  sref <- newIORef $ State { stLastTime = t
                            , stWindow = window }
   mainLoop sref
 
@@ -78,75 +81,45 @@ fromTime :: Sound -> Time -> Sound
 snd `fromTime` startTime = \t -> snd (t + startTime)
 
 ----------------------------------------------------------------------------------------------------
-createChunkFromSound :: Int -> Double -> Sound -> IO SoundChunk
-createChunkFromSound sampleRate seconds snd = do
+--
+-- I'm going to change this function so that it doesn't take seconds as an argument but
+-- takes a number of bytes to produce instead.
+--
+
+{-# INLINE createChunkFromSound #-}
+createChunkFromSound :: Ptr Word8 -> Int -> Int -> Sound -> Time -> IO ()
+createChunkFromSound buf sampleRate bytes sound startTime = do
   t <- getCurrentTime
-  let numSamples = ceiling (fromIntegral sampleRate * seconds)
-      bytes = numSamples * outputChannels * bytesPerSample
-      sampleSizeInSeconds = seconds / fromIntegral sampleRate
+  let numSamples = bytes `div` (outputChannels * bytesPerSample)
+      sampleSizeInSeconds = 1.0 / (fromIntegral sampleRate)
       scale :: Int
       scale = 2^15 - 1
       -- a block is the number of bytes for one sample per channel.
       blockSize = bytesPerSample*outputChannels
-  (abuf :: Ptr Word8) <- mallocBytes bytes
   -- printf "numSamples = %d, bytes = %d\n" numSamples bytes
-  forM_ [0..numSamples-1] $ \i -> do
-    let n = fromIntegral i * sampleSizeInSeconds
-        sample = snd n         -- now sample snd at n
-        scaledSample :: Int16
-        scaledSample = ceiling (fromIntegral scale *  sample)
-    forM_ [0..outputChannels-1] $ \j -> do
-      pokeByteOff abuf (i*blockSize+j*bytesPerSample) scaledSample
-  let chunkStruct = M.ChunkStruct 1 abuf (fromIntegral bytes) 128
-  chunkPtr <- malloc
-  poke chunkPtr chunkStruct
-  chunk <- M.mkFinalizedChunk chunkPtr
+  let loop i | i < 0 || i >= numSamples = return ()
+             | otherwise = do
+                let n = fromIntegral i * sampleSizeInSeconds
+                    sample = sound (n + startTime)         -- now sample snd at n
+                    scaledSample :: Int16
+                    scaledSample = floor (fromIntegral scale * sample)
+                forM_ [0..outputChannels-1] $ \j -> do
+                  pokeByteOff buf (i*blockSize+j*bytesPerSample) scaledSample
+                loop (i+1)
+  loop 0
   t' <- getCurrentTime
-  printf "Chunk creation: %.2fs\n" $ (realToFrac $ diffUTCTime t' t :: Double)
-  return $ SoundChunk { scChunk = chunk, scDuration = seconds }
+  printf "Chunk creation: %.4fs, bytes: %d\n" (realToFrac $ diffUTCTime t' t :: Double) bytes
+  return ()
 
 --------------------------------------------------------------------------------------------------
 mainLoop :: IORef State -> IO ()
 mainLoop sref = do
   t <- getCurrentTime
-  -- S.delay 10 -- So we don't consume 100% CPU
+  S.delay 1000 -- So we don't consume 100% CPU
   mbEvent <- S.pollEvent
   case mbEvent of
     Just e | checkForQuit e -> exitWith ExitSuccess
     _ -> return ()
-
-  let playNextChunk = do
-        t <- getCurrentTime
-        mbNextChunk <- do
-          s <- readIORef sref
-          case stChunkQueue s of
-            []             -> do
-              writeIORef sref $ s { stCurrentChunk = Nothing }
-              return Nothing
-            (chunk:chunks) -> do
-              putStrLn $ show t
-              writeIORef sref $ s { stChunkQueue = chunks
-                                  , stCurrentChunk = Just chunk
-                                  , stCurrentChunkElapsed = 0 }
-              return $ Just chunk
-        maybe (return ()) (\sc -> M.playChannelTimed (-1) (scChunk sc) 0 1000 >> return ())
-              mbNextChunk
-
-  -- Pull the next chunk off the queue and play it, but only if one isn't playing.
-  s <- readIORef sref
-  case stCurrentChunk s of
-    Nothing -> playNextChunk
-    Just sc -> do
-      let elapsed = stCurrentChunkElapsed s
---      printf "Elapsed time: %.2f\n" elapsed
-      if elapsed >= scDuration sc
-       then writeIORef sref $ s { stCurrentChunk = Nothing }
-       else do
-         -- update duration
-         let d = realToFrac $ diffUTCTime t (stLastTime s)
-         modifyIORef sref $ \s -> s { stCurrentChunkElapsed = elapsed + d }
-
-
   -- Update the last time each time through the loop
   modifyIORef sref $ \s -> s { stLastTime = t }
   mainLoop sref
