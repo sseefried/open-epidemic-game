51f7a0ce62d13399e9d5cfceb86d7224b01e5475
Sean Seefried <sean.seefried@gmail.com>
2015-03-04 10:11:03 +1100

    Update script to also build the library

--- update-ios-simulator.sh
+++ update-ios-simulator.sh
@@ -5,6 +5,12 @@ if [ "$EPIDEMIC_IOS_BUILD_I386_LIB_DIR" = "" ]; then
   exit 1
 fi
 
+i386-apple-darwin11-cabal configure -fios && \
+  i386-apple-darwin11-cabal build exe:EpidemicStaticLib
+[ $? -eq 0 ] || exit 1
+
+echo
 echo Copying to Epidemic to $EPIDEMIC_IOS_BUILD_I386_LIB_DIR
+echo
 cp dist/i386-apple-darwin11/build/EpidemicStaticLib/EpidemicStaticLib.a \
   "$EPIDEMIC_IOS_BUILD_I386_LIB_DIR/libEpidemic.a"
